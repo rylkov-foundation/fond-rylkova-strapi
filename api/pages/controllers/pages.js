8a7c63f6deb857395ac3219ec7fb966ae16d2a25
freebeego <freebeego@yandex.ru>
2021-04-08 16:44:15 +0300

    fix(main): remove not required fields from main response

--- api/pages/controllers/pages.js
+++ api/pages/controllers/pages.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { sanitizeEntity } = require('strapi-utils');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
@@ -7,12 +9,27 @@
 
 module.exports = {
   async main(ctx) {
+    const dirtyResults = sanitizeEntity(
+      await strapi.services['our-results'].find(),
+      { model: strapi.models.about }
+      ).results;
+
+    const footer = sanitizeEntity(
+      await strapi.services.footer.find(),
+      { model: strapi.models.about }
+      );
+
+    const results = dirtyResults.map(result => sanitizeEntity(result, { model: strapi.models.result }));
+
+    footer.Social_links = footer.Social_links.map(link => ({ link: link.Link, iconUrl: link.Icon.url }));
+    footer.Foreign_agent_link = { link: footer.Foreign_agent_link.Link, iconUrl: footer.Foreign_agent_link.Icon.url };
+
     return {
-      about: await strapi.services.about.find(),
-      mission: await strapi.services.mission.find(),
-      results: await strapi.services['our-results'].find(),
-      help: await strapi.services.help.find(),
-      footer: await strapi.services.footer.find(),
+      about: sanitizeEntity(await strapi.services.about.find(), { model: strapi.models.about }),
+      mission: sanitizeEntity(await strapi.services.mission.find(), { model: strapi.models.about }),
+      results,
+      help: sanitizeEntity(await strapi.services.help.find(), { model: strapi.models.about }),
+      footer
     };
   },
 };
