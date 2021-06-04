24b9f512b7c3a672b32508814964bb9cdb561509
freebeego <freebeego@yandex.ru>
2021-06-04 12:26:31 +0300

    fix: slug toLowerCase

--- api/page/models/page.js
+++ api/page/models/page.js
@@ -6,12 +6,12 @@ module.exports = {
   lifecycles: {
     beforeCreate: async (data) => {
       if (data.title_en) {
-        data.slug = slugify(data.title_en);
+        data.slug = slugify(data.title_en.toLowerCase());
       }
     },
     beforeUpdate: async (params, data) => {
       if (data.title_en) {
-        data.slug = slugify(data.title_en);
+        data.slug = slugify(data.title_en.toLowerCase());
       }
     },
   },
