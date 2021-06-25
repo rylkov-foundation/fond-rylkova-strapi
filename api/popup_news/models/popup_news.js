d1f4f681440c375dfb23b4f1febb538aee31bf61
freebeego <freebeego@yandex.ru>
2021-06-25 21:04:27 +0300

    fix: news popup

--- api/popup_news/models/popup_news.js
+++ api/popup_news/models/popup_news.js
@@ -1,8 +1,19 @@
 'use strict';
 
+const uuid = require('uuid');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      data.newsId = uuid();
+    },
+    beforeUpdate: async (params, data) => {
+      data.newsId = uuid();
+    }
+  }
+};
