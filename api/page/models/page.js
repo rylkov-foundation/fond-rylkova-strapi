d26fd88c2da161613a7e2c835e1c8fdcd3b022bf
freebeego <freebeego@yandex.ru>
2021-07-17 13:34:27 +0300

    feat: replace creator on publish with updater

--- api/page/models/page.js
+++ api/page/models/page.js
@@ -14,5 +14,28 @@ module.exports = {
         data.slug = slugify(data.title_en.toLowerCase());
       }
     },
+    afterUpdate: async (result) => {
+      const AUTHOR = 'strapi-author'
+      const roles = (
+        await strapi.query('role', 'admin').find({})
+      )
+        .reduce(
+          (acc, role) => ({ ...acc, [role.id]: role.code }),
+          {}
+        )
+      const documentCreatorRoles = result.created_by.roles.map((role) => role.toString())
+      if (
+        result.published_at &&
+        documentCreatorRoles.some((role) => roles[role] === AUTHOR)
+      ) {
+        await strapi
+          .query('page')
+          .model
+          .updateOne(
+            { _id: result.id },
+            { created_by: result.updated_by.id.toString() }
+          )
+      }
+    }
   },
 };
