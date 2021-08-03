ef3b25272789af821103373f9aab267a9a9ce645
freebeego <freebeego@yandex.ru>
2021-08-03 19:55:27 +0300

    fix: remove download popup and fix server config

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'localhost'),
+  url: env('URL', ''),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '431346c9188e00e7aa5203146bbed1ad'),
